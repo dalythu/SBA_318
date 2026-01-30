c3be1c7e8dcbf4696f979e2a38f550583e13d992
dalythu <dalythu@gmail.com>
2026-01-29 22:20:31 -0500

    added example data

---
+++ db/data.js
@@ -0,0 +1,14 @@
+export const students = [
+  { id: 1, name: "Thurston" },
+  { id: 2, name: "Daly" },
+]
+
+export const classes = [
+  { id: 101, name: "Math" },
+  { id: 102, name: "Science" },
+]
+
+export const grades = [
+  { id: 1, studentId: 1, classId: 101, score: 95 },
+  { id: 2, studentId: 2, classId: 102, score: 88 },
+]
