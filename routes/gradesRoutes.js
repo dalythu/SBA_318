3757800d27f27065a5095326daaae1089799aacb
dalythu <dalythu@gmail.com>
2026-01-29 23:04:08 -0500

    Added GETs for grades by id

--- routes/gradesRoutes.js
+++ routes/gradesRoutes.js
@@ -1,11 +1,21 @@
 import express from 'express'
+import grades from '../db/grades.js'
 
 const router = express.Router()
 
+// GET all grades
 router.get('/', (req, res) => {
-  res.json({
-    message: 'Grade routes are working',
-  })
+  res.json(grades)
 })
+// GET one grade by id
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const grade = grades.find((g) => g.id === id)
 
+  if (!grade) {
+    return res.status(404).json({ error: 'Grade not found' })
+  }
+
+  res.json(grade)
+})
 export default router
